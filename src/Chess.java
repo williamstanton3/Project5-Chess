98bcd32b1b441cf43b4c63a56643d8ef4d925c8f
STANTONWJ23 <stantonWJ23@gcc.edu>
2024-04-23 11:39:59 -0400

    Initial commit

---
+++ src/Chess.java
@@ -0,0 +1,10 @@
+public class Chess {
+
+    // idea: we have to use two different kinds of collections
+    // we can use a list (arraylist probably) easily for the game
+    // i think we should use a map to track which users have won which times
+    // kind of like an ultimate chess game that takes the users names before the game
+    // and stores the winner in a map with an int that represents the number of times they have won
+
+    // another question: how will we print the chess symbols on the game board? and the colors?
+}
---
+++ src/scratch.java
@@ -0,0 +1,18 @@
+
+
+public class scratch {
+    public static void main(String[] args) {
+//        for (int x = 9812; x<=9823; x++) {
+//            System.out.println((char)x);
+//            System.out.println("hey");
+//        }
+
+        char blackKing = (char)9812;
+        System.out.println(blackKing);
+
+
+        char blackQueen = (char)9813;
+        System.out.println(blackQueen);
+
+    }
+}
